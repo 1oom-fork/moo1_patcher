a2f1b5dbb3b28dd9aab925980b43a1221910d4cd
MyName <fork1oom@yandex.com>
2025-07-09 07:06:41 +0300

    delay_switch: Add fix for orion.exe

--- delay_switch.c
+++ delay_switch.c
@@ -13,11 +13,21 @@ int main(int argc, char **argv)
     };
     int len0 = 1;
     int off0 = 0x3bc22;
+    int off1 = 0x235c6;
     const patch_t patch_set[] = {
         {match0, replace0, off0, len0},
         {NULL, NULL, 0, 0},
     };
+    const patch_t patch_set2[] = {
+        {match0, replace0, off1, len0},
+        {NULL, NULL, 0, 0},
+    };
+
     patch_status_t result = execute_patcher("STARMAP.EXE", patch_set);
+    if (result == PATCH_STATUS_INVALID) {
+        printf("Fail\n");
+    }
+    result = execute_patcher("ORION.EXE", patch_set2);
     if (result == PATCH_STATUS_INVALID) {
         printf("Fail\n");
     }
